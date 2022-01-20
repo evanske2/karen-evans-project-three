90e7c072c888d7862ea0f97e19b289273a1aa00b
Evans <evanske2@gmail.com>
2022-01-19 23:23:31 -0500

    added noreferrer to links

--- src/components/PageFooter.js
+++ src/components/PageFooter.js
@@ -4,8 +4,8 @@ const PageFooter = () => {
   return (
     <footer>
       <div className="wrapper">
-        <p>Created at <a href="https://junocollege.com/" target="_blank">Juno College</a></p>
-        <p>Data provided by <a href="https://developer.marvel.com/" target="_blank">Marvel</a>. &copy; 2014 <a href="https://developer.marvel.com/" target="_blank">Marvel</a></p>
+        <p>Created at <a href="https://junocollege.com/" target="_blank" rel="noreferrer">Juno College</a></p>
+        <p>Data provided by <a href="https://developer.marvel.com/" target="_blank" rel="noreferrer">Marvel</a>. &copy; 2014 <a href="https://developer.marvel.com/" target="_blank" rel="noreferrer">Marvel</a></p>
         <p><span>Excelsior!</span></p>
       </div>
     </footer>
